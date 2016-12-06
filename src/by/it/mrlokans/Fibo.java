d7d7e1345a0eee1006e1e5df514dbe75c0644ac1
MrLokans <trikster1911@gmail.com>
2016-12-06 08:50:49 +0300

    Added fast implementation of fibonacci number calculation.

--- src/by/it/mrlokans/Fibo.java
+++ src/by/it/mrlokans/Fibo.java
@@ -8,6 +8,7 @@ import java.math.BigInteger;
 public class Fibo {
 
     private long startTime = System.currentTimeMillis();
+    private static int FIBO_MATRIX_INDEX = 1;
 
     private long time() {
         return System.currentTimeMillis() - startTime;
@@ -79,7 +80,43 @@ public class Fibo {
 
         //попробуйте здесь релизовать самый быстрый и эффективный по использованию памяти
         //вариант, какой только сумеете
-        return BigInteger.ZERO;
+
+        // Here we use fibonacci number calcaulation algorithm, based
+        // on the matrix exponentiation
+        // see https://en.wikipedia.org/wiki/Fibonacci_number#Matrix_form
+        BigInteger[] initialMatrix = new BigInteger[]{
+            BigInteger.ONE, BigInteger.ONE, BigInteger.ONE, BigInteger.ZERO
+        };
+
+        BigInteger[] calculatedMatrix = exponentiateMatrix(initialMatrix, n);
+        return calculatedMatrix[FIBO_MATRIX_INDEX];
+    }
+
+    public static BigInteger[] exponentiateMatrix(BigInteger[] matrix, int power){
+        BigInteger[] result = {BigInteger.ONE, BigInteger.ZERO, BigInteger.ZERO, BigInteger.ONE};
+
+        // Here we use exponentiation by squaring to optimize calculations
+        // https://en.wikipedia.org/wiki/Exponentiation_by_squaring
+        while (power != 0){
+            if (power % 2 != 0){
+                result = multiplySquareMatrices(result, matrix);
+            }
+            power /= 2;
+            matrix = multiplySquareMatrices(matrix, matrix);
+        }
+        return result;
+    }
+
+    // Multiplies two 2x2 square matrices
+    public static BigInteger[] multiplySquareMatrices(BigInteger[] first, BigInteger[] second){
+        // multiplies row by column
+        BigInteger[] result = new BigInteger[] {
+            first[0].multiply(second[0]).add(first[1].multiply(second[2])),
+            first[0].multiply(second[1]).add(first[1].multiply(second[3])),
+            first[2].multiply(second[0]).add(first[3].multiply(second[2])),
+            first[2].multiply(second[1]).add(first[3].multiply(second[3]))
+        };
+        return result;
     }
 
 
