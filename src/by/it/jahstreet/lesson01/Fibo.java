a68d2b3a861bfc3f74c045ce161d3cac573aa19c
aleksandrsosnovskih <s-a100500>
2016-12-07 18:48:23 +0300

    lesson01 & lesson02 start

---
+++ src/by/it/jahstreet/lesson01/Fibo.java
@@ -0,0 +1,67 @@
+package by.it.jahstreet.lesson01;
+
+import java.math.BigInteger;
+
+/*
+ * Вам необходимо выполнить три способа вычисления чисел Фибоначчи.
+ */
+public class Fibo {
+
+    private long startTime = System.currentTimeMillis();
+
+    private long time() {
+        return System.currentTimeMillis() - startTime;
+    }
+
+    public static void main(String[] args) {
+        Fibo fibo = new Fibo();
+        int n = 33;
+        System.out.printf("calc(%d)=%d \n\t time=%d \n\n", n, fibo.calc(n), fibo.time());
+
+        //вычисление чисел фибоначчи медленным методом (рекурсией)
+        fibo = new Fibo();
+        n = 33;
+        System.out.printf("slowA(%d)=%d \n\t time=%d \n\n", n, fibo.slowA(n), fibo.time());
+
+        //вычисление чисел простым быстрым методом
+        fibo = new Fibo();
+        n = 55555;
+        System.out.printf("fastB(%d)=%d \n\t time=%d \n\n", n, fibo.fastB(n), fibo.time());
+
+        //вычисление чисел фибоначчи самым быстрым методом
+        fibo = new Fibo();
+        n = 55555;
+        System.out.printf("fasterC(%d)=%d \n\t time=%d \n\n", n, fibo.fasterC(n), fibo.time());
+    }
+
+
+    private int calc(int n) {
+        //здесь простейший вариант, в котором код совпадает с мат.определением чисел Фибоначчи
+        //время O(2^n)
+        return 0;
+    }
+
+
+    BigInteger slowA(Integer n) {
+        //рекурсия
+        //здесь нужно реализовать вариант без ограничения на размер числа,
+        //в котором код совпадает с мат.определением чисел Фибоначчи
+        //время O(2^n)
+
+        return BigInteger.ZERO;
+    }
+
+    BigInteger fastB(Integer n) {
+        //здесь нужно реализовать вариант с временем O(n) и памятью O(n)
+        return BigInteger.ZERO;
+    }
+
+    BigInteger fasterC(Integer n) {
+
+        //попробуйте здесь релизовать самый быстрый и эффективный по использованию памяти
+        //вариант, какой только сумеете
+        return BigInteger.ZERO;
+    }
+
+
+}
