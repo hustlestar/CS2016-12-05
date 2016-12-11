f7c718ab9248020428aaf02d300fa4fe2e0d726f
MrLokans <trikster1911@gmail.com>
2016-12-11 20:42:52 +0300

    Lesson 3: Huffman algorithm added.

--- src/by/it/mrlokans/lesson3/A_Huffman.java
+++ src/by/it/mrlokans/lesson3/A_Huffman.java
@@ -4,39 +4,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
 
-//Lesson 3. A_Huffman.
-//Разработайте метод encode(File file) для кодирования строки (код Хаффмана)
-
-// По данным файла (непустой строке ss длины не более 104104),
-// состоящей из строчных букв латинского алфавита,
-// постройте оптимальный по суммарной длине беспрефиксный код.
-
-// Используйте Алгоритм Хаффмана — жадный алгоритм оптимального
-// безпрефиксного кодирования алфавита с минимальной избыточностью.
-
-// В первой строке выведите количество различных букв kk,
-// встречающихся в строке, и размер получившейся закодированной строки.
-// В следующих kk строках запишите коды букв в формате "letter: code".
-// В последней строке выведите закодированную строку. Примеры ниже
-
-//        Sample Input 1:
-//        a
-//
-//        Sample Output 1:
-//        1 1
-//        a: 0
-//        0
-
-//        Sample Input 2:
-//        abacabad
-//
-//        Sample Output 2:
-//        4 14
-//        a: 0
-//        b: 10
-//        c: 110
-//        d: 111
-//        01001100100111
 
 public class A_Huffman {
 
@@ -87,7 +54,6 @@ public class A_Huffman {
 
     }
 
-    ////////////////////////////////////////////////////////////////////////////////////
     //расширение базового класса до листа дерева
     private class LeafNode extends Node {
         //лист
@@ -112,29 +78,48 @@ public class A_Huffman {
 
     //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
     String encode(File file) throws FileNotFoundException {
-        //прочитаем строку для кодирования из тестового файла
         Scanner scanner = new Scanner(file);
         String s = scanner.next();
 
-        //все комментарии от тестового решения были оставлены т.к. это задание A.
-        //если они вам мешают их можно удалить
-
         Map<Character, Integer> count = new HashMap<>();
-        //1. переберем все символы по очереди и рассчитаем их частоту в Map count
-            //для каждого символа добавим 1 если его в карте еще нет или инкремент если есть.
 
-        //2. перенесем все символы в приоритетную очередь в виде листьев
+        for(int i=0; i< s.length(); i++){
+            char ch = s.charAt(i);
+            if (!count.containsKey(ch)){
+                count.put(ch, 1);
+            } else {
+                count.put(ch, count.get(ch) + 1);
+            }
+        }
+
         PriorityQueue<Node> priorityQueue = new PriorityQueue<>();
 
         //3. вынимая по два узла из очереди (для сборки родителя)
         //и возвращая этого родителя обратно в очередь
         //построим дерево кодирования Хаффмана.
         //У родителя частоты детей складываются.
+        for (Map.Entry<Character, Integer> element: count.entrySet()){
+            LeafNode leaf = new LeafNode(element.getValue(), element.getKey());
+            priorityQueue.add(leaf);
+        }
 
+        // We need to have one node remaining
+        while (priorityQueue.size() > 1){
+            Node leftNode = priorityQueue.poll();
+            Node rightNode = priorityQueue.poll();
+            InternalNode node = new InternalNode(leftNode, rightNode);
+            priorityQueue.add(node);
+        }
+
+        priorityQueue.poll().fillCodes("");
         //4. последний из родителей будет корнем этого дерева
         //это будет последний и единственный элемент оставшийся в очереди priorityQueue.
         StringBuilder sb = new StringBuilder();
-        //.....
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            sb.append(codes.get(ch));
+        }
 
         return sb.toString();
         //01001100100111
