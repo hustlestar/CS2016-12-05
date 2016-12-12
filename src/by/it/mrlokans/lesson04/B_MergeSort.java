a467782b3837ea2217fdf2647194e62e049fddf7
MrLokans <trikster1911@gmail.com>
2016-12-12 20:42:04 +0300

    Lesson 4: Merge sort implementation added.

--- src/by/it/mrlokans/lesson04/B_MergeSort.java
+++ src/by/it/mrlokans/lesson04/B_MergeSort.java
@@ -21,6 +21,46 @@ Sample Output:
 */
 public class B_MergeSort {
 
+    public static int[] mergeSort(int[] a){
+        // recursion exit condition
+        if (a.length <= 1){
+            return a;
+        }
+
+        // splitting array in half
+        int[] leftHalf = new int[a.length / 2];
+        int[] rightHalf = new int[a.length - a.length / 2];
+
+        // Copy left array elements
+        System.arraycopy(a, 0, leftHalf, 0, leftHalf.length);
+        // Copy right array elements
+        System.arraycopy(a, 0, rightHalf, 0, rightHalf.length);
+
+        mergeSort(leftHalf);
+        mergeSort(rightHalf);
+
+        merge(leftHalf, rightHalf, a);
+        return a;
+    }
+
+    public static void merge(int[] left, int[] right, int[] merged){
+        int leftIndex = 0, rightIndex = 0;
+        int resultIndex = 0;
+
+        while(leftIndex < left.length && rightIndex < right.length){
+            if (left[leftIndex] < right[rightIndex]){
+                merged[resultIndex] = left[leftIndex];
+                leftIndex++;
+            } else {
+                merged[resultIndex] = rightIndex;
+                rightIndex++;
+            }
+            resultIndex++;
+        }
+        System.arraycopy(left, leftIndex, merged, resultIndex, left.length - leftIndex);
+        System.arraycopy(right, rightIndex, merged, resultIndex, right.length - rightIndex);
+    }
+
     int[] getMergeSort(InputStream stream) throws FileNotFoundException {
         //подготовка к чтению данных
         Scanner scanner = new Scanner(stream);
@@ -39,8 +79,7 @@ public class B_MergeSort {
 
 
 
-
-
+        mergeSort(a);
 
         //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
         return a;
