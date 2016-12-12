b4d7784df2194e8ab19c84ae359be4cca8b6b0d4
Andrei Dushkou <andrew.burn1ng@gmail.com>
2016-12-12 23:01:07 +0300

    a, b, c finished

---
+++ src/by/it/dushkov/lesson01/Fibo.java
@@ -0,0 +1,94 @@
+package by.it.dushkov.lesson01;
+
+import java.math.BigInteger;
+
+/*
+ * Вам необходимо выполнить три способа вычисления чисел Фибоначчи.
+ */
+public class Fibo {
+
+    private long startTime = System.currentTimeMillis();
+
+    private long time() {
+        return System.currentTimeMillis() - startTime;
+    }
+
+    public static void main(String[] args) {
+        Fibo fibo = new Fibo();
+        int n = 33;
+        System.out.printf("calc(%d)=%d \n\t time=%d \n\n", n, fibo.calc(n), fibo.time());
+
+        //вычисление чисел фибоначчи медленным методом (рекурсией)
+        fibo = new Fibo();
+        n = 33;
+        System.out.printf("slowA(%d)=%d \n\t time=%d \n\n", n, fibo.slowA(n), fibo.time());
+
+        //вычисление чисел простым быстрым методом
+        fibo = new Fibo();
+        n = 55555;
+        System.out.printf("fastB(%d)=%d \n\t time=%d \n\n", n, fibo.fastB(n), fibo.time());
+
+        //вычисление чисел фибоначчи самым быстрым методом
+        fibo = new Fibo();
+        n = 55555;
+        System.out.printf("fasterC(%d)=%d \n\t time=%d \n\n", n, fibo.fasterC(n), fibo.time());
+    }
+
+
+    private int calc(int n) {
+        //здесь простейший вариант, в котором код совпадает с мат.определением чисел Фибоначчи
+        //время O(2^n)
+        int fiboPrev = 0;
+        int fiboNext = 1;
+        int fibo = 0; //initialize
+        for (int i = 2; i <= n; i++) {
+            fibo = fiboPrev + fiboNext;
+            fiboPrev = fiboNext;
+            fiboNext = fibo;
+        }
+        return fibo;
+    }
+
+
+    BigInteger slowA(Integer n) {
+        //рекурсия
+        //здесь нужно реализовать вариант без ограничения на размер числа,
+        //в котором код совпадает с мат.определением чисел Фибоначчи
+        //время O(2^n)
+        if (n == 0) return BigInteger.ZERO;
+        if (n == 1) return BigInteger.ONE;
+        return slowA(n - 1).add(slowA(n - 2)); //recursion
+    }
+
+    BigInteger fastB(Integer n) {
+        //здесь нужно реализовать вариант с временем O(n) и памятью O(n)
+        BigInteger[] arr = new BigInteger[n+1];
+        arr[0] = BigInteger.valueOf(0);
+        arr[1] = BigInteger.valueOf(1);
+
+        for (int i = 2; i < arr.length; i++) {
+            arr[i] = arr[i-1].add(arr[i-2]); //each next = sum of prev and prev.prev
+        }
+        return arr[n];
+    }
+
+    BigInteger fasterC(Integer n) {
+
+        //попробуйте здесь релизовать самый быстрый и эффективный по использованию памяти
+        //вариант, какой только сумеете
+
+        BigInteger num = BigInteger.valueOf(0);
+
+        BigInteger num1 = BigInteger.valueOf(0);
+        BigInteger num2 = BigInteger.valueOf(1);
+
+        for (int i = 2; i <= n; i++) {
+            num = num1.add(num2);
+            num1 = num2;
+            num2 = num;
+        }
+        return num;
+    }
+
+
+}
