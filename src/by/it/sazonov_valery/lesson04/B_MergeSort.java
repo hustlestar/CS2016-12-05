11c293f954339552cfa13c8e50845f9ce58309e7
Valery Sazonov <sazmaster@gmail.com>
2016-12-13 14:32:10 +0300

    lesson 4 - task_B - done

--- src/by/it/sazonov_valery/lesson04/B_MergeSort.java
+++ src/by/it/sazonov_valery/lesson04/B_MergeSort.java
@@ -3,6 +3,7 @@ package by.it.sazonov_valery.lesson04;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.Scanner;
 
 /*
@@ -29,31 +30,66 @@ public class B_MergeSort {
         //размер массива
         int n = scanner.nextInt();
         //сам массива
-        int[] a=new int[n];
+        int[] a = new int[n];
         for (int i = 1; i <= n; i++) {
-            a[i-1] = scanner.nextInt();
+            a[i - 1] = scanner.nextInt();
         }
         // тут ваше решение (реализуйте сортировку слиянием)
         // https://ru.wikipedia.org/wiki/Сортировка_слиянием
 
 
+        int kr = 1; // кратность сравнений
+        int shift; // сдвиг в перебираемом массиве
+        int two_size; // количество елементов для 2-го массива
+        int[] arr2;
+        while (kr < n) {
+            shift = 0;
+            while (shift < n) {
+                if (shift + kr >= n) break;
 
-
-
-
+                if (shift + kr * 2 > n){
+                    two_size = n - (shift + kr);
+                } else {
+                    two_size = kr;
+                }
+                arr2 = mergeArrays(Arrays.copyOfRange(a, shift, shift + kr),
+                        Arrays.copyOfRange(a, shift + kr, shift + kr + two_size));
+                for (int i = 0; i < kr + two_size; ++i)
+                    a[shift + i] = arr2[i]; // замена на отсортированное
+                shift += kr * 2;
+            }
+            kr *= 2;
+        }
 
         //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
         return a;
     }
+    private static int[] mergeArrays(int[] arr_1, int[] arr_2) {
+        int len_1 = arr_1.length, len_2 = arr_2.length;
+        int a = 0, b = 0, len = len_1 + len_2; // a, b - счетчики в массивах
+        int[] result = new int[len];
+        for (int i = 0; i < len; i++) {
+            if (b < len_2 && a < len_1) {
+                if (arr_1[a] > arr_2[b]) result[i] = arr_2[b++];
+                else result[i] = arr_1[a++];
+            } else if (b < len_2) {
+                result[i] = arr_2[b++];
+            } else {
+                result[i] = arr_1[a++];
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) throws FileNotFoundException {
         String root = System.getProperty("user.dir") + "/src/";
         InputStream stream = new FileInputStream(root + "by/it/a_khmelov/lesson04/dataB.txt");
         B_MergeSort instance = new B_MergeSort();
         //long startTime = System.currentTimeMillis();
-        int[] result=instance.getMergeSort(stream);
+        int[] result = instance.getMergeSort(stream);
         //long finishTime = System.currentTimeMillis();
-        for (int index:result){
-            System.out.print(index+" ");
+        for (int index : result) {
+            System.out.print(index + " ");
         }
     }
 
