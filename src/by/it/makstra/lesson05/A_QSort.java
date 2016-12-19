586c3b55f5974ba18a0392645590904d7557c2f6
maksTra <p.matthew@mail.ru>
2016-12-19 20:04:06 +0300

    lesson 5

--- src/by/it/makstra/lesson05/A_QSort.java
+++ src/by/it/makstra/lesson05/A_QSort.java
@@ -3,6 +3,8 @@ package by.it.makstra.lesson05;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 /*
@@ -11,18 +13,14 @@ import java.util.Scanner;
 Известны данные о том, когда каждая из них включалась и выключалась (отрезки работы)
 Известен список событий на площади (время начала каждого события).
 Вам необходимо определить для каждого события сколько камер его записали.
-
 В первой строке задано два целых числа:
     число включений камер (отрезки) 1<=n<=50000
     число событий (точки) 1<=m<=50000.
-
 Следующие n строк содержат по два целых числа ai и bi (ai<=bi) -
 координаты концов отрезков (время работы одной какой-то камеры).
 Последняя строка содержит m целых чисел - координаты точек.
 Все координаты не превышают 10E8 по модулю (!).
-
 Точка считается принадлежащей отрезку, если она находится внутри него или на границе.
-
 Для каждой точки в порядке их появления во вводе выведите,
 скольким отрезкам она принадлежит.
     Sample Input:
@@ -32,13 +30,12 @@ import java.util.Scanner;
     1 6 11
     Sample Output:
     1 0 0
-
 */
 
 public class A_QSort {
 
     //отрезок
-    private class Segment  implements Comparable{
+    private class Segment  implements Comparable<Segment>{
         int start;
         int stop;
 
@@ -50,9 +47,9 @@ public class A_QSort {
         }
 
         @Override
-        public int compareTo(Object o) {
+        public int compareTo(Segment o) {
             //подумайте, что должен возвращать компаратор отрезков
-            return 0;
+            return Integer.compare(this.start, o.start);
         }
     }
 
@@ -75,19 +72,87 @@ public class A_QSort {
             segments[i]=new Segment(scanner.nextInt(),scanner.nextInt());
         }
         //читаем точки
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < m; i++) {
             points[i]=scanner.nextInt();
         }
         //тут реализуйте логику задачи с применением быстрой сортировки
         //в классе отрезка Segment реализуйте нужный для этой задачи компаратор
 
+        //Мои комментарии:
+        //Реализованное мною решение предполагает следующее:
+        //Сортируем массив отрезков segments[] по возрастанию;
+        //В качестве сравниваемого значения указываем переменную start;
+        //Данная манипуляция позволяет при вводе в наивный код конструкции "if(points[i]<segments[j].start) break;"
+        //иногда значительно сократить количество действий над массивами по сравнению с наивным алгоритмом(в зависимости от входных данных),
+        //избавив программу от необходимости проверять наличие точки в отрезках, переменная start которых
+        //будет однозначно больше переменной start, которая проверялась на условие, т.к. массив отрезков отсортирован
+        //по возрастанию
 
+        //Arrays.sort(segments);
+        //вместо того, чтобы воспользоваться готовым статическим методом класса Arrays,
+        //пишем свой кустарный метод быстрой сортировки qSort
 
+        segments = qSort(segments, 0, n-1);
 
-        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        for(Segment x:segments)
+            System.out.println("["+x.start+" : "+x.stop+"]");
+
+        //часть кода для проверки принадлежности точки отрезкам
+
+        for(int i =0; i<m; i++)
+        {
+            int count=0;
+            for(int j=0;j<n;j++)
+            {
+                if(segments[j].start<=points[i] && points[i]<=segments[j].stop)
+                {
+                    count++;
+                }
+                else if(points[i]<segments[j].start) break;
+            }
+            result[i] = count;
+        }
         return result;
     }
 
+    //Реализация быстрой сортировки:
+    public static Segment[] qSort(Segment segs[], int left, int right) {
+        int i = left;
+        int j = right;
+        Random rand = new Random();
+        Segment x = segs[i + rand.nextInt(j - i + 1)];
+        while(i<=j)
+        {
+            while(segs[i].compareTo(x)==(-1))
+            {
+                i++;
+            }
+            while(segs[j].compareTo(x)==1)
+            {
+                j--;
+            }
+            if(i<=j)
+            {
+                Segment t = segs[i];
+                segs[i] = segs[j];
+                segs[j] = t;
+                i++;
+                j--;
+            }
+        }
+        if (left<j)
+        {
+            qSort(segs, left, j);
+        }
+        if (i<right)
+        {
+            qSort(segs, i, right);
+        }
+        return segs;
+    }
+
+
+    //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
 
     public static void main(String[] args) throws FileNotFoundException {
         String root = System.getProperty("user.dir") + "/src/";
--- src/by/it/makstra/lesson05/B_CountSort.java
+++ src/by/it/makstra/lesson05/B_CountSort.java
@@ -8,11 +8,7 @@ import java.util.Scanner;
 /*
 Первая строка содержит число 1<=n<=10000, вторая - n натуральных чисел, не превышающих 10.
 Выведите упорядоченную по неубыванию последовательность этих чисел.
-
 При сортировке реализуйте метод со сложностью O(n)
-
-Пример: https://karussell.wordpress.com/2010/03/01/fast-integer-sorting-algorithm-on/
-Вольный перевод: http://programador.ru/sorting-positive-int-linear-time/
 */
 
 public class B_CountSort {
@@ -30,25 +26,52 @@ public class B_CountSort {
         for (int i = 0; i < n; i++) {
             points[i]=scanner.nextInt();
         }
-        //тут реализуйте логику задачи с применением сортировки подсчетом
 
-
-
-
-
-        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
-        return points;
+        return cSort(points);
     }
 
+    //метод для сортировки подсчётом(подходит только для сортировки неотрицательных значений)
+    /*
+    1.Находим максимальное и минимальное значения массива
+    2.Создаём вспомогательный массив counter[], где каждый counter[i]=x; i равно одному из значений исходного массива, из
+    которого вычли минимальное значение, а x - количество повторений этого значения.
+    3.Далее, используя массив counter[], переписываем исходный массив, делая его отсортированным.
+     */
+    public static int[] cSort(int[] array) {
+        int min, max = min = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+            }
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+
+        int[] counter = new int[max - min + 1];
+        for (int i = 0; i < array.length; i++) {
+            counter[array[i] - min]++;
+        }
+
+        int idCounter = 0;
+        for (int i = 0; i < counter.length; i++) {
+            for (int j = 0; j < counter[i]; j++) {
+                array[idCounter] = i + min;
+                idCounter++;
+            }
+        }
+        return array;
+    }
+
+    //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
 
     public static void main(String[] args) throws FileNotFoundException {
         String root = System.getProperty("user.dir") + "/src/";
-        InputStream stream = new FileInputStream(root + "by/it/a_khmelov/lesson05/dataB.txt");
+        InputStream stream = new FileInputStream("C:\\Documents and Settings\\traenovichM\\IdeaProjects\\untitled\\src\\countSort\\dataB.txt");
         B_CountSort instance = new B_CountSort();
         int[] result=instance.countSort(stream);
         for (int index:result){
             System.out.print(index+" ");
         }
     }
-
 }
