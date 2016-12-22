9f31607bf5b20c546b6067c35bfbc473bfba4bba
a_ozerov <andrei.ozerov@gmail.com>
2016-12-22 23:09:54 +0300

    Lesson 1  A, B, C

--- src/by/it/a_ozerov/lesson01/Fibo.java
+++ src/by/it/a_ozerov/lesson01/Fibo.java
@@ -1,6 +1,8 @@
 package by.it.a_ozerov.lesson01;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 
 /*
  * Вам необходимо выполнить три способа вычисления чисел Фибоначчи.
@@ -55,33 +57,46 @@ public class Fibo {
         //здесь нужно реализовать вариант без ограничения на размер числа,
         //в котором код совпадает с мат.определением чисел Фибоначчи
         //время O(2^n)
+
         if (n <= 1) {
             return BigInteger.valueOf(n);
         }
-
         return slowA(n - 2).add(slowA(n - 1));
     }
 
         BigInteger fastB(Integer n) {
             //здесь нужно реализовать вариант с временем O(n) и памятью O(n)
-            BigInteger[] arrayFibo = new BigInteger[n];
-            arrayFibo[0] = BigInteger.ZERO;
-            arrayFibo[1] = BigInteger.ONE;
+            List<BigInteger> fiboList = new ArrayList<BigInteger>();
+            fiboList.add(BigInteger.ZERO);
+            fiboList.add(BigInteger.ONE);
 
-            for (int i = 2; i <= (n - 1); i++) {
-                arrayFibo[i] = arrayFibo[i - 1].add(arrayFibo[i - 2]);
+            for (int i = 2; i <= n; i++) {
+                fiboList.add(fiboList.get(i - 2).add(fiboList.get(i - 1)));
             }
-
-            return arrayFibo [n - 1];
+            return fiboList.get(n - 1);
         }
 
                 BigInteger fasterC(Integer n) {
 
                     //попробуйте здесь релизовать самый быстрый и эффективный по использованию памяти
                     //вариант, какой только сумеете
-                    return BigInteger.ZERO;
+                    BigInteger fibo0 = BigInteger.ZERO;
+                    BigInteger fibo1 = BigInteger.ONE;
+
+                    if (n > 1) {
+                        for (int i = 2; i < n; i++) {
+                            if (i % 2 == 0) {
+                                fibo0 = fibo0.add(fibo1);
+                            } else {
+                                fibo1 = fibo1.add(fibo0);
+                            }
+                        }
+                    }
+
+                    if (n % 2 ==0){
+                        return fibo0;
+                    } else {
+                        return fibo1;
+                    }
                 }
-
-
             }
-
