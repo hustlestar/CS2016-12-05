06e160089dd3aa239f8236e09b2b79d6602da3eb
a_ozerov <andrei.ozerov@gmail.com>
2016-12-25 01:29:03 +0300

    Lesson 4  A, B, C

--- src/by/it/a_ozerov/lesson03/C_HeapMax.java
+++ src/by/it/a_ozerov/lesson03/C_HeapMax.java
@@ -43,22 +43,53 @@ public class C_HeapMax {
         //Будет мало? Ну тогда можете его собрать как Generic и/или использовать в варианте B
         private List<Long> heap = new ArrayList<>();
 
-        int siftDown(int i) { //просеивание вверх
-
+        int siftDown(int i) { //просеивание вниз
+            while ((2 * i + 1 < heap.size())){
+                int left  = 2 * i + 1;
+                int right  = 2 * i + 2;
+                int j = left;
+                if (right < heap.size() && heap.get(right) > heap.get(left))
+                    j = right;
+                if (heap.get(i) >= heap.get(j))
+                    break;
+                Long tmp = heap.get(i);
+                heap.set(i, heap.get(j));
+                heap.set(j, tmp);
+                i = j;
+            }
             return i;
         }
 
-        int siftUp(int i) { //просеивание вниз
-
+        int siftUp(int i) { //просеивание вверх
+            while (i > 0 && heap.get(i) > heap.get((i - 1)/2)){
+                int j = (i - 1)/2;
+                Long tmp = heap.get(i);
+                heap.set(i, heap.get(j));
+                heap.set(j, tmp);
+                i = j;
+            }
             return i;
         }
 
         void insert(Long value) { //вставка
+            heap.add(value);
+            siftUp(heap.size()-1);
         }
 
         Long extractMax() { //извлечение и удаление максимума
             Long result = null;
+            if (heap.size() > 0){
+                result = heap.get(0);
+                if (heap.size() > 1) {
+                    int last = heap.size() - 1;
+                    heap.set(0, heap.get(last));
+                    heap.remove(last);
+                    siftDown (0);
+                }
+                else
+                    heap.remove(0); //heap.size() - 1
 
+            }
             return result;
         }
         //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! КОНЕЦ ЗАДАЧИ !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
