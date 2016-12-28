5b6e3e1b520c313b01c14756159cca4a8d385adb
veronicaDemesh <vnd-vendetta@mail.ru>
2016-12-29 02:00:54 +0300

    Veronica Demesh's new folder
    It contains:
    -lesson1: a,b,c;
    -lesson2: a,b,c;
    -lesson3: a,b,c;
    -lesson4: a,b;
    -lesson5: a,b;
    -lesson6: a,b;
    -lesson8: a,b,c.

---
+++ src/by/it/vDemesh_new/lesson08/B_EditDist.java
@@ -0,0 +1,85 @@
+package by.it.vDemesh_new.lesson08;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+/*
+Задача на программирование: расстояние Левенштейна
+    https://ru.wikipedia.org/wiki/Расстояние_Левенштейна
+    http://planetcalc.ru/1721/
+
+Дано:
+    Две данных непустые строки длины не более 100, содержащие строчные буквы латинского алфавита.
+
+Необходимо:
+    Решить задачу МЕТОДАМИ ДИНАМИЧЕСКОГО ПРОГРАММИРОВАНИЯ
+    Итерационно вычислить расстояние редактирования двух данных непустых строк
+
+    Sample Input 1:
+    ab
+    ab
+    Sample Output 1:
+    0
+
+    Sample Input 2:
+    short
+    ports
+    Sample Output 2:
+    3
+
+    Sample Input 3:
+    distance
+    editing
+    Sample Output 2:
+    5
+
+*/
+
+public class B_EditDist {
+    
+    int getDistanceEdinting(String one, String two) {
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        char [] word1 = one.toCharArray();
+        char [] word2 = two.toCharArray();
+        int[][] dist = new int[word1.length+1][word2.length+1];
+
+        for (int i = 0; i <= word1.length; i++) {
+            dist[i][0] = i;
+        }
+        for (int j = 0; j <= word2.length; j++) {
+            dist[0][j] = j;
+        }
+
+        for (int i = 1; i <= word1.length; i++) {
+            for (int j = 1; j <= word2.length; j++) {
+                if (i == 0) {
+                    dist[i][j] = j;
+                } else if (j == 0) {
+                    dist[i][j] = i;
+                } else {
+                    int dif = (word1[i - 1] == word2[j - 1]) ? 0 : 1;
+                    int ins = dist[i - 1][j] + 1;
+                    int del = dist[i][j - 1] + 1;
+                    int rep = dist[i - 1][j - 1] + dif;
+                    dist[i][j] = Math.min(Math.min(ins, del), rep);
+                }
+            }
+        }
+
+        return dist[word1.length][word2.length];
+    }
+
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String root = System.getProperty("user.dir") + "/src/";
+        InputStream stream = new FileInputStream(root + "by/it/vDemesh_new/lesson08/dataABC.txt");
+        B_EditDist instance = new B_EditDist();
+        Scanner scanner = new Scanner(stream);
+        System.out.println(instance.getDistanceEdinting(scanner.nextLine(),scanner.nextLine()));
+        System.out.println(instance.getDistanceEdinting(scanner.nextLine(),scanner.nextLine()));
+        System.out.println(instance.getDistanceEdinting(scanner.nextLine(),scanner.nextLine()));
+    }
+
+}
