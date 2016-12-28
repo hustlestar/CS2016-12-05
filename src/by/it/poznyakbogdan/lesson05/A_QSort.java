0aaa99beaecc99a42b6936d17e03b8fbf4307303
Bogdan Poznyak <poznyak.bogdan@gmail.com>
2016-12-28 23:43:19 +0300

    5 lesson task A,B,C; A in development.

--- src/by/it/poznyakbogdan/lesson05/A_QSort.java
+++ src/by/it/poznyakbogdan/lesson05/A_QSort.java
@@ -1,9 +1,9 @@
 package by.it.poznyakbogdan.lesson05;
 
- import java.io.FileInputStream;
- import java.io.FileNotFoundException;
- import java.io.InputStream;
- import java.util.Scanner;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Scanner;
 
 /*
 Видеорегистраторы и площадь.
@@ -11,14 +11,18 @@ package by.it.poznyakbogdan.lesson05;
 Известны данные о том, когда каждая из них включалась и выключалась (отрезки работы)
 Известен список событий на площади (время начала каждого события).
 Вам необходимо определить для каждого события сколько камер его записали.
+
 В первой строке задано два целых числа:
     число включений камер (отрезки) 1<=n<=50000
     число событий (точки) 1<=m<=50000.
+
 Следующие n строк содержат по два целых числа ai и bi (ai<=bi) -
 координаты концов отрезков (время работы одной какой-то камеры).
 Последняя строка содержит m целых чисел - координаты точек.
 Все координаты не превышают 10E8 по модулю (!).
+
 Точка считается принадлежащей отрезку, если она находится внутри него или на границе.
+
 Для каждой точки в порядке их появления во вводе выведите,
 скольким отрезкам она принадлежит.
     Sample Input:
@@ -28,155 +32,134 @@ package by.it.poznyakbogdan.lesson05;
     1 6 11
     Sample Output:
     1 0 0
+
 */
 
 public class A_QSort {
 
     //отрезок
-    private class Point implements Comparable<Point> {
-        int x;
-        int index;
-        int p;
+    private class Segment  implements Comparable{
+        int start;
+        int stop;
 
-        Point(int x, int index) {
-            this.x = x;
-            this.index = index;
+        Segment(int start, int stop){
+            if (start > stop){
+                this.start = stop;
+                this.stop = start;
+            } else {
+                this.start = start;
+                this.stop = stop;
+            }
             //тут вообще-то лучше доделать конструктор на случай если
             //концы отрезков придут в обратном порядке
         }
 
         @Override
-        public int compareTo(Point o) {
-            return x - o.x;
+        public int compareTo(Object o) {
+            //подумайте, что должен возвращать компаратор отрезков
+            return 0;
+        }
+    }
+    private class Point{
+        int start;
+        boolean type;
+
+        Point(int start, boolean type){
+            this.start = start;
+            this.type = type;
+        }
+
+    }
+
+
+    void swap(Point[] array, int a, int b){
+        Point temp = array[a];
+        array[a] = array[b];
+        array[b] = temp;
+    }
+
+    int partition(Point[] array, int begin, int end){
+        int m = begin;
+        int i = begin + 1;
+        int j = end;
+        int result = begin;
+        while (i < j){
+            if (array[i].start >= array[m].start){
+                boolean flag = true;
+                while (flag == true && i <= j){
+                    if (array[j].start < array[m].start){
+                        swap(array, i, j);
+                        result++;
+                        j--;
+                        flag = false;
+                    }else{
+                        j--;
+                    }
+                }
+            }else {
+                result++;
+            }
+            i++;
+        }
+        swap(array, result, m);
+        return result;
+    }
+
+    private void quickSort(Point[] ar, int begin, int end){
+        if (begin >= end){
+
+        }else {
+            int m = partition(ar, begin, end);
+            quickSort(ar, begin, m - 1);
+            quickSort(ar, m + 1, end);
         }
     }
 
     int[] getAccessory(InputStream stream) throws FileNotFoundException {
-        Scanner sc = new Scanner(stream);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int[] result = new int[m];
-        Point[] points = new Point[2 * n + m];
+        //подготовка к чтению данных
+        Scanner scanner = new Scanner(stream);
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        //число отрезков отсортированного массива
+        int n = scanner.nextInt();
+        Segment[] segments=new Segment[n];
+        Point[] dots = new Point[n*2];
+        //число точек
+        int m = scanner.nextInt();
+        int[] points=new int[m];
+        int[] result=new int[m];
+
+        //читаем сами отрезки
+        for (int i = 0; i < n; i++) {
+            //читаем начало и конец каждого отрезка
+            segments[i]=new Segment(scanner.nextInt(),scanner.nextInt());
+            dots[2*i] = new Point(segments[i].start, true);
+            dots[2*i + 1] = new Point(segments[i].stop, false);
+
+        }
+        //читаем точки
+        for (int i = 0; i < m; i++) {
+            points[i]=scanner.nextInt();
+        }
+        //тут реализуйте логику задачи с применением быстрой сортировки
+        //в классе отрезка Segment реализуйте нужный для этой задачи компаратор
+        quickSort(dots, 0, dots.length - 1);
+        for (int i = 0; i < dots.length; i++) {
+            System.out.println(dots[i].start);
+        }
 
 
-        for (int i = 0; i < 2 * n; i++) {
-            int start = sc.nextInt();
-            int finish = sc.nextInt();
-            if (start < finish) {
-                points[i] = new Point(start, -1);
-                points[++i] = new Point(finish, points.length);
-            } else {
-                points[i] = new Point(finish, -1);
-                points[++i] = new Point(start, points.length);
-            }
-        }
-        int l = 0;
-        for (int i = 2 * n; i < 2 * n + m; i++) {
-            points[i] = new Point(sc.nextInt(), l++);
-        }
-        qSort(points);
-        int camNo = 0;
-        for (Point point : points) {
-            if (point.index < 0) {
-                camNo++;
-            } else if (point.index == points.length) {
-                camNo--;
-            } else {
-                result[point.index] = camNo;
-            }
-        }
+        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
         return result;
-    }
-
-    private void qSort(Point[] a) {
-        quickSort(a, 0, a.length - 1);
-    }
-
-    private static void quickSort(Point[] array, int low, int high) {
-        if (array == null || array.length == 0) {
-            return;
-        }
-
-        if (low >= high) {
-            return;
-        }
-
-        int middle = low + (high - low) / 2;
-        Point pivot = array[middle];
-
-        int i = low, j = high;
-        while (i <= j) {
-            while (array[i].x < pivot.x) {
-                i++;
-            }
-
-            while (array[j].x > pivot.x) {
-                j--;
-            }
-            if (i <= j) {
-                Point temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
-                i++;
-                j--;
-            }
-        }
-        if (low < j)
-            quickSort(array, low, j);
-
-        if (high > i)
-            quickSort(array, i, high);
-    }
-
-    private int[] numbers;
-    private int number;
-
-    public void sort(int[] values) {
-        if (values == null || values.length == 0) {
-            return;
-        }
-        this.numbers = values;
-        number = values.length;
-        quickSort(0, number - 1);
-    }
-
-
-    private void quickSort(int low, int high) {
-        int i = low, j = high;
-        int pivot = numbers[low + (high - low) / 2];
-        while (i <= j) {
-            while (numbers[i] < pivot) {
-                i++;
-            }
-            while (numbers[j] > pivot) {
-                j--;
-            }
-            if (i <= j) {
-                swap(i, j);
-                i++;
-                j--;
-            }
-        }
-        if (low < j)
-            quickSort(low, j);
-        if (i < high)
-            quickSort(i, high);
-    }
-
-    private void swap(int i, int j) {
-        int temp = numbers[i];
-        numbers[i] = numbers[j];
-        numbers[j] = temp;
     }
 
 
     public static void main(String[] args) throws FileNotFoundException {
         String root = System.getProperty("user.dir") + "/src/";
-        InputStream stream = new FileInputStream(root + "by/it/du4/lesson05/dataA.txt");
+        InputStream stream = new FileInputStream(root + "by/it/poznyakbogdan/lesson05/dataA.txt");
         A_QSort instance = new A_QSort();
-        int[] result = instance.getAccessory(stream);
-        for (int index : result) {
-            System.out.print(index + " ");
+        int[] result=instance.getAccessory(stream);
+        for (int index:result){
+            System.out.print(index+" ");
         }
     }
 
