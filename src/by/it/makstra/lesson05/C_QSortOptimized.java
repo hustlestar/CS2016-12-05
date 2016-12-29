c35fa1758aa9487747f5e3c94a6408e9e5bbb933
maksTra <p.matthew@mail.ru>
2016-12-29 11:16:03 +0300

    lesson 8(c) & lesson 5(c)

--- src/by/it/makstra/lesson05/C_QSortOptimized.java
+++ src/by/it/makstra/lesson05/C_QSortOptimized.java
@@ -3,6 +3,7 @@ package by.it.makstra.lesson05;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.Random;
 import java.util.Scanner;
 
 /*
@@ -33,19 +34,26 @@ import java.util.Scanner;
 public class C_QSortOptimized {
 
     //отрезок
-    private class Segment  implements Comparable{
-        int start;
-        int stop;
+    private class Point implements Comparable<Point>{
+        int x;
+        int index;
 
-        Segment(int start, int stop){
-            this.start = start;
-            this.stop = stop;
+        public Point(int x, int index) {
+            this.x = x;
+            this.index = index;
         }
 
         @Override
-        public int compareTo(Object o) {
-            //подумайте, что должен возвращать компаратор отрезков
-            return 0;
+        public int compareTo(Point o) {
+            //подумайте, что должен возвращать компаратор
+            if(x != o.x) return x-o.x;
+            else return(index - o.index);
+        }
+
+        @Override
+        public String toString() {
+            String p = "x:"+x+" ,ind:"+index;
+            return p;
         }
     }
 
@@ -54,30 +62,109 @@ public class C_QSortOptimized {
         //подготовка к чтению данных
         Scanner scanner = new Scanner(stream);
         //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        //тут реализуйте логику задачи с применением быстрой сортировки
+
+        //реализованное мною решение предполагает следующее:
+        //Создаём массив точек, в который закладываем точки начала отрезка(присваиваем таким точкам индекс 0),
+        //точки конца отрезков(присваиваем таким точкам индекс m+1, где m-число точек, принадлежность которых надо найти),
+        // и непосредственно сами точки, принадлежность которых надо найти. Индекс таких точек находися в пределах [1,m]
+        // в зависимости от порядкового номера.
+        //Так сделано для удобства поиска этих точек после того, как массив будет отсортирован по
+        //возрастанию, ведь в качестве ответа нам надо дать количество пересечений с отрезком неотсортированной точки.
+
         //число отрезков отсортированного массива
         int n = scanner.nextInt();
-        Segment[] segments=new Segment[n];
+
         //число точек
         int m = scanner.nextInt();
-        int[] points=new int[m];
-        int[] result=new int[m];
+
+        //массив точек
+        Point[] points = new Point[2 * n + m];
+
+        //массив ответов
+        int[] result = new int[m];
 
         //читаем сами отрезки
-        for (int i = 0; i < n; i++) {
-            //читаем начало и конец каждого отрезка
-            segments[i]=new Segment(scanner.nextInt(),scanner.nextInt());
+        for (int i = 0; i < 2 * n; i++) {
+            //читаем начало отрезка
+            points[i++] = new Point(scanner.nextInt(), 0);
+            //читаем конец отрезка
+            points[i] = new Point(scanner.nextInt(), m+1);
         }
         //читаем точки
-        for (int i = 0; i < n; i++) {
-            points[i]=scanner.nextInt();
+        for (int i = 2 * n; i < (2 * n + m); i++) {
+            points[i] = new Point(scanner.nextInt(), i-2*n+1);
         }
-        //тут реализуйте логику задачи с применением быстрой сортировки
-        //в классе отрезка Segment реализуйте нужный для этой задачи компаратор
 
+        //Arrays.sort(points);//вместо использования статического метода класса Arrays напишем свой(реализация ниже)
+        qSortUp(points,0,2*n+m-1);
 
-        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        for (Point x:points){
+            System.out.println(x);}
+
+        //Теперь за один проход по массиву точек мы можем дать ответ по этой задаче.
+        // Индексы помогут нам определить зарактер точки: Входная, выходная или искомая.
+        // Если точка входная counter++, если выходная counter--, если искомая то заносим
+        // в массив ответов result в ячейку "index-1"(-1 т.к. во время вычислений произошло
+        // смещение массива на единицу) текущее значение counter.
+
+        int counter = 0;
+
+        for (int i = 0; i < (2 * n + m); i++) {
+            if (points[i].index == 0) counter++;
+            else
+            if (points[i].index > m) counter--;
+            else {
+                result[points[i].index-1] = counter;
+            }
+        }
         return result;
     }
+
+    private void swap (Point[] a, int i, int j)
+    {
+        Point t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    //Реализация быстрой сортировки:
+    public Point[] qSortUp(Point p[], int l, int r) {
+        while(l<r)
+        {
+            int mid[] = partition(p,l,r);
+            qSortUp(p,l,mid[0]-1);
+            l = mid[1]+1;
+        }
+        return p;
+    }
+
+    private int[] partition(Point[] p, int l, int r)
+    {
+        Random rnd = new Random();
+        int random = l+rnd.nextInt(r-l);
+        int pivot = l;
+        int pivotEnd = l;
+        swap(p, pivot, random);
+        for(int i = l+1; i<=r;i++)
+        {
+            if(p[i].compareTo(p[pivot])<=0) {
+                pivotEnd++;
+                swap(p,i,pivotEnd);
+                if(p[pivotEnd].compareTo(p[pivot])<0)
+                {
+                    swap(p,pivotEnd,pivot);
+                    pivot++;
+                }
+            }
+
+        }
+        int part[] = {pivot, pivotEnd};
+        return part;
+    }
+
+
+    //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
 
 
     public static void main(String[] args) throws FileNotFoundException {
